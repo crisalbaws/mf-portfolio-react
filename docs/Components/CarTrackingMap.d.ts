4e6c97185b2d0c0311c4c512078fdca38afdf5cf
crisalbaws <146770231+crisalbaws@users.noreply.github.com>
2024-11-12 11:00:42 -0600

    deploy

---
+++ docs/Components/CarTrackingMap.d.ts
@@ -0,0 +1,4 @@
+import React from 'react';
+import 'leaflet/dist/leaflet.css';
+declare const LeafletMap: React.FC;
+export default LeafletMap;
---
+++ docs/Components/Utils.d.ts
@@ -0,0 +1,6 @@
+export declare const demoProducts: {
+    id: number;
+    name: string;
+    price: number;
+    imageUrl: string;
+}[];
